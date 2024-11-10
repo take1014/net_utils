b2fe7b48c7b3b72e6f7dc750708e5531e2c3dba1
take <y.takehara1014@gmail.com>
2024-11-11 00:08:42 +0900

    add tensorflow net_utils.py

---
+++ tf/net_utils.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env/python3
+import tensorflow as tf
+from tensorflow.keras import layers
+
+class LinearRelu(layers.Layer):
+    def __init__(self, units, bias=True, name=None):
+        super(LinearRelu, self).__init__(name=name)
+        self.linear = layers.Dense(units=units, use_bias=bias, name=f"_{name}_linear")
+        self.relu = layers.ReLU(name=f"_{name}_relu")
+
+    def call(self, inputs):
+        x = self.linear(inputs)
+        return self.relu(x)
+
+class LinearBnRelu(layers.Layer):
+    def __init__(self, units, bias=True, name=None):
+        super(LinearBnRelu, self).__init__(name=name)
+        self.linear = layers.Dense(units=units, use_bias=bias, name=f"_{name}_linear")
+        self.bn = layers.BatchNormalization(name=f"_{name}_bn")
+        self.relu = layers.ReLU(name=f"_{name}_relu")
+
+    def call(self, inputs):
+        x = self.linear(inputs)
+        x = self.bn(x)
+        return self.relu(x)
+
+class LinearBn(layers.Layer):
+    def __init__(self, units, bias=True, name=None):
+        super(LinearBn, self).__init__(name=name)
+        self.linear = layers.Dense(units=units, use_bias=bias, name=f"_{name}_linear")
+        self.bn = layers.BatchNormalization(name=f"_{name}_bn")
+
+    def call(self, inputs):
+        x = self.linear(inputs)
+        return self.bn(x)
+
+class ResLinearBlock(layers.Layer):
+    def __init__(self, in_units, out_units, bias=True, name=None):
+        super(ResLinearBlock, self).__init__(name=name)
+        self.linear_relu = LinearRelu(units=out_units, bias=bias, name=f"_{name}_linear_relu")
+        self.linear1 = layers.Dense(units=out_units, use_bias=bias, name=f"_{name}_linear1")
+        self.linear2 = None
+        if in_units != out_units:
+            self.linear2 = layers.Dense(units=out_units, use_bias=bias, name=f"_{name}_linear2")
+        self.relu = layers.ReLU(name=f"_{name}_relu")
+
+    def call(self, inputs):
+        x2 = self.linear_relu(inputs)
+        x2 = self.linear1(x2)
+        x1 = inputs if self.linear2 is None else self.linear2(inputs)
+        return self.relu(layers.Add(name=f"_{self.name}_add")([x1, x2]))
+
+class ResLinearBlockWithBn(layers.Layer):
+    def __init__(self, in_units, out_units, bias=True, name=None):
+        super(ResLinearBlockWithBn, self).__init__(name=name)
+        self.linear_bn_relu = LinearBnRelu(units=out_units, bias=bias, name=f"_{name}_linear_bn_relu")
+        self.linear_bn1 = LinearBn(units=out_units, bias=bias, name=f"_{name}_linear_bn1")
+        self.linear_bn2 = None
+        if in_units != out_units:
+            self.linear_bn2 = LinearBn(units=out_units, bias=bias, name=f"_{name}_linear_bn2")
+        self.relu = layers.ReLU(name=f"_{name}_relu")
+
+    def call(self, inputs):
+        x2 = self.linear_bn_relu(inputs)
+        x2 = self.linear_bn1(x2)
+        x1 = inputs if self.linear_bn2 is None else self.linear_bn2(inputs)
+        return self.relu(layers.Add(name=f"_{self.name}_add")([x1, x2]))
+
+class Conv2dBn(layers.Layer):
+    def __init__(self, filters, kernel_size=(3,3), stride=(1,1), padding='valid', dilation_rate=(1,1), bias=True, name=None):
+        super(Conv2dBn, self).__init__(name=name)
+        self.conv2d = layers.Conv2D(filters=filters, kernel_size=kernel_size, strides=stride, padding=padding, dilation_rate=dilation_rate, use_bias=bias, name=f"_{name}_conv2d")
+        self.bn = layers.BatchNormalization(name=f"_{name}_bn")
+
+    def call(self, inputs):
+        x = self.conv2d(inputs)
+        return self.bn(x)
+
+class Conv2dBnRelu(layers.Layer):
+    def __init__(self, filters, kernel_size=(3,3), stride=(1,1), padding='valid', dilation_rate=(1,1), bias=True, name=None):
+        super(Conv2dBnRelu, self).__init__(name=name)
+        self.conv2d_bn = Conv2dBn(filters=filters, kernel_size=kernel_size, stride=stride, padding=padding, dilation_rate=dilation_rate, bias=bias, name=f"_{name}_conv2d_bn")
+        self.relu = layers.ReLU(name=f"_{name}_relu")
+
+    def call(self, inputs):
+        x = self.conv2d_bn(inputs)
+        return self.relu(x)
+
+class Conv2dRelu(layers.Layer):
+    def __init__(self, filters, kernel_size=(3,3), stride=(1,1), padding='valid', dilation_rate=(1,1), bias=True, name=None):
+        super(Conv2dRelu, self).__init__(name=name)
+        self.conv2d = layers.Conv2D(filters=filters, kernel_size=kernel_size, strides=stride, padding=padding, dilation_rate=dilation_rate, use_bias=bias, name=f"_{name}_conv2d")
+        self.relu = layers.ReLU(name=f"_{name}_relu")
+
+    def call(self, inputs):
+        x = self.conv2d(inputs)
+        return self.relu(x)
+
+class ResConvBlock(layers.Layer):
+    def __init__(self, in_filters, out_filters, kernel_size=(3,3), stride=(1,1), padding='same', dilation_rate=(1,1), bias=True, name=None):
+        super(ResConvBlock, self).__init__(name=name)
+        self.conv2d_relu = Conv2dRelu(filters=out_filters, kernel_size=kernel_size, stride=stride, padding=padding, dilation_rate=dilation_rate, bias=bias, name=f"_{name}_conv2d_relu")
+        self.conv2d1 = layers.Conv2D(filters=out_filters, kernel_size=kernel_size, strides=(1,1), padding=padding, dilation_rate=dilation_rate, use_bias=bias, name=f"_{name}_conv2d1")
+        self.conv2d2 = None
+        if stride != (1,1) or in_filters != out_filters:
+            self.conv2d2 = layers.Conv2D(filters=out_filters, kernel_size=(1,1), strides=stride, padding=padding, dilation_rate=dilation_rate, use_bias=bias, name=f"_{name}_conv2d2")
+        self.relu = layers.ReLU(name=f"_{name}_relu")
+
+    def call(self, inputs):
+        x2 = self.conv2d_relu(inputs)
+        x2 = self.conv2d1(x2)
+        x1 = inputs if self.conv2d2 is None else self.conv2d2(inputs)
+        return self.relu(layers.Add(name=f"_{self.name}_add")([x1, x2]))
+
+class ResConvBlockWithBn(layers.Layer):
+    def __init__(self, in_filters, out_filters, kernel_size=(3,3), stride=(1,1), padding='same', dilation_rate=(1,1), bias=True, name=None):
+        super(ResConvBlockWithBn, self).__init__(name=name)
+        self.conv2d_bn_relu = Conv2dBnRelu(filters=out_filters, kernel_size=kernel_size, stride=stride, padding=padding, dilation_rate=dilation_rate, bias=bias, name=f"_{name}_conv2d_bn_relu")
+        self.conv2d_bn1 = Conv2dBn(filters=out_filters, kernel_size=kernel_size, stride=(1,1), padding=padding, dilation_rate=dilation_rate, bias=bias, name=f"_{name}_conv2d_bn1")
+        self.conv2d_bn2 = None
+        if stride != (1,1) or in_filters != out_filters:
+            self.conv2d_bn2 = Conv2dBn(filters=out_filters, kernel_size=(1,1), stride=stride, bias=bias, name=f"_{name}_conv2d_bn2")
+        self.relu = layers.ReLU(name=f"_{name}_relu")
+
+    def call(self, inputs):
+        x2 = self.conv2d_bn_relu(inputs)
+        x2 = self.conv2d_bn1(x2)
+        x1 = inputs if self.conv2d_bn2 is None else self.conv2d_bn2(inputs)
+        return self.relu(layers.Add(name=f"_{self.name}_add")([x1, x2]))
+
+class GlobalAvgPool2d(layers.Layer):
+    def __init__(self, name=None):
+        super(GlobalAvgPool2d, self).__init__(name=name)
+        self.pooling = layers.GlobalAveragePooling2D(name=f"_{name}_pooling")
+
+    def call(self, inputs):
+        return self.pooling(inputs)
--- torch/net_utils.py
+++ torch/net_utils.py
@@ -8,8 +8,8 @@ class LinearRelu(nn.Module):
         self.linear = nn.Linear(in_features=in_features, out_features=out_features, bias=bias)
         self.relu   = nn.ReLU()
 
-    def forward(self, x):
-        x = self.linear(x)
+    def forward(self, inputs):
+        x = self.linear(inputs)
         return self.relu(x)
 
 class LinearBnRelu(nn.Module):
@@ -19,8 +19,8 @@ class LinearBnRelu(nn.Module):
         self.bn     = nn.BatchNorm2d(num_features=out_features)
         self.relu   = nn.ReLU()
 
-    def forward(self, x):
-        x = self.linear(x)
+    def forward(self, inputs):
+        x = self.linear(inputs)
         x = self.bn(x)
         return self.relu(x)
 
@@ -30,8 +30,8 @@ class LinearBn(nn.Module):
         self.linear = nn.Linear(in_features=in_features, out_features=out_features, bias=bias)
         self.bn     = nn.BatchNorm2d(num_features=out_features)
 
-    def forward(self, x):
-        x = self.linear(x)
+    def forward(self, inputs):
+        x = self.linear(inputs)
         return self.bn(x)
 class ResLinearBlock(nn.Module):
     def __init__(self, in_features, out_features, bias=True):
@@ -43,10 +43,10 @@ class ResLinearBlock(nn.Module):
             self.linear2 = nn.Linear(in_features=in_features, out_features=out_features, bias=bias)
         self.relu   = nn.ReLU()
 
-    def forward(self, x):
-        x2 = self.linear_relu(x)
+    def forward(self, inputs):
+        x2 = self.linear_relu(inputs)
         x2 = self.linear1(x2)
-        x1 = x if self.linear2 is None else self.lienar2(x)
+        x1 = inputs if self.linear2 is None else self.lienar2(inputs)
         return self.relu(torch.add(x1, x2))
 
 class ResLinearBlockWithBn(nn.Module):
@@ -59,10 +59,10 @@ class ResLinearBlockWithBn(nn.Module):
             self.linear_bn2 = LinearBn(in_features=in_features, out_features=out_features, bias=bias)
         self.relu   = nn.ReLU()
 
-    def forward(self, x):
-        x2 = self.linear_bn_relu(x)
+    def forward(self, inputs):
+        x2 = self.linear_bn_relu(inputs)
         x2 = self.bn(self.linear1(x2))
-        x1 = x if self.linear_bn2 is None else self.linear_bn2(x)
+        x1 = inputs if self.linear_bn2 is None else self.linear_bn2(inputs)
         return self.relu(torch.add(x1, x2))
 
 class Conv2dBn(nn.Module):
@@ -71,8 +71,8 @@ class Conv2dBn(nn.Module):
         self.conv2d = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, bias=bias)
         self.bn     = nn.BatchNorm2d(num_features=out_channels)
 
-    def forward(self, x):
-        x = self.conv2d(x)
+    def forward(self, inputs):
+        x = self.conv2d(inputs)
         return self.bn(x)
 
 class Conv2dBnRelu(nn.Module):
@@ -81,8 +81,8 @@ class Conv2dBnRelu(nn.Module):
         self.conv2d_bn = Conv2dBn(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, bias=bias)
         self.relu   = nn.ReLU()
 
-    def forward(self, x):
-        x = self.conv2d_bn(x)
+    def forward(self, inputs):
+        x = self.conv2d_bn(inputs)
         return self.relu(x)
 
 class Conv2dRelu(nn.Module):
@@ -91,8 +91,8 @@ class Conv2dRelu(nn.Module):
         self.conv2d = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, bias=bias)
         self.relu   = nn.ReLU()
 
-    def forward(self, x):
-        x = self.conv2d(x)
+    def forward(self, inputs):
+        x = self.conv2d(inputs)
         return self.relu(x)
 
 class ResConvBlock(nn.Module):
@@ -105,10 +105,10 @@ class ResConvBlock(nn.Module):
             self.conv2d2 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=(1,1), stride=stride, padding=padding, dilation=dilation, bias=bias)
         self.relu   = nn.ReLU()
 
-    def forward(self, x):
-        x2 = self.conv2d_relu1(x)
+    def forward(self, inputs):
+        x2 = self.conv2d_relu1(inputs)
         x2 = self.conv2d1(x2)
-        x1 =  x if self.conv2d2 is None else self.conv2d2(x)
+        x1 =  inputs if self.conv2d2 is None else self.conv2d2(inputs)
         return self.relu(torch.add(x1, x2))
 
 class ResConvBlockWithBn(nn.Module):
@@ -121,10 +121,10 @@ class ResConvBlockWithBn(nn.Module):
             self.conv2d_bn2 = Conv2dBn(in_channels=in_channels, out_channels=out_channels, kernel_size=(1,1), stride=stride, bias=bias)
         self.relu = nn.ReLU()
 
-    def forward(self, x):
-        x2 = self.conv2d_bn_relu(x)
+    def forward(self, inputs):
+        x2 = self.conv2d_bn_relu(inputs)
         x2 = self.conv2d_bn1(x2)
-        x1 = x if self.conv2d_bn2 is None else self.conv2d_bn2(x)
+        x1 = inputs if self.conv2d_bn2 is None else self.conv2d_bn2(inputs)
         return self.relu(torch.add(x1, x2))
 
 class DeConvRelu(nn.Module):
@@ -133,8 +133,8 @@ class DeConvRelu(nn.Module):
         self.deconv2d = nn.ConvTranspose2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, bias=bias)
         self.relu   = nn.ReLU()
 
-    def forward(self, x):
-        x = self.deconv2d(x)
+    def forward(self, inputs):
+        x = self.deconv2d(inputs)
         return self.relu(x)
 
 class DeConvBnRelu(nn.Module):
@@ -144,8 +144,8 @@ class DeConvBnRelu(nn.Module):
         self.bn     = nn.BatchNorm2d(num_features=out_channels)
         self.relu   = nn.ReLU()
 
-    def forward(self, x):
-        x = self.deconv2d(x)
+    def forward(self, inputs):
+        x = self.deconv2d(inputs)
         x = self.bn(x)
         return self.relu(x)
 
@@ -154,7 +154,7 @@ class GlobalAvgPool2D(nn.Module):
         super(GlobalAvgPool2D, self).__init__()
         self.gap = nn.AdaptiveAvgPool2d((1,1))
 
-    def forward(self, x):
-        x = self.gap(x)
+    def forward(self, inputs):
+        x = self.gap(inputs)
         # Convert (N, C, 1, 1) to (N, C)
         return x.view(x.size(0), -1)
