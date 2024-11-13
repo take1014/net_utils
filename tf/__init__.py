a684dafb5562beb50b21f51b08c3137c8fedc909
take <y.takehara1014@gmail.com>
2024-11-13 21:25:57 +0900

    add init to tf

---
+++ tf/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env/python3
+from .net_utils import (
+    LinearRelu, LinearBnRelu, LinearBn, ResLinearBlock, ResLinearBlockWithBn,
+    Conv2dBn, Conv2dBnRelu, Conv2dRelu, ResConvBlock, ResConvBlockWithBn,
+    DeConvRelu, DeConvBnRelu, GlobalAvgPool2D
+)
