057299c4705cd673338a58ee87a3b381e5f65c27
take <y.takehara1014@gmail.com>
2024-11-13 21:24:29 +0900

    update __init__.py

---
+++ torch/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env/python3
+from .net_utils import (
+    LinearRelu, LinearBnRelu, LinearBn, ResLinearBlock, ResLinearBlockWithBn,
+    Conv2dBn, Conv2dBnRelu, Conv2dRelu, ResConvBlock, ResConvBlockWithBn,
+    DeConvRelu, DeConvBnRelu, GlobalAvgPool2D
+)
+from .backborn import ResNet18
